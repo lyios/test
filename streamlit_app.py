764737ca2e7cccb2a152db8acc4d05db1197e5e2
lyios <muichiro2104@gmail.com>
2024-07-18 08:59:53 +0000

    ｆ

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,6 @@ st.title('点数表示')
 
 st.write('あなたの点数を教えてください')
 
-number = st.number_input('国語', 50)
-number = st.number_input('数学', 50)
-number = st.number_input('社会', 50)
-number = st.number_input('理科', 50)
-number = st.number_input('英語', 50)
-
 categories = ['国語', '数学', '社会', '理科','英語']
 
 # スライダーで値を変更
