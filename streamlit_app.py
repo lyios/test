5dd6e9011c80d13eadff523df4efed383d2de0f8
lyios <muichiro2104@gmail.com>
2024-07-18 08:09:46 +0000

    r

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ st.title('点数表示')
 
 st.write('あなたの点数を教えてください')
 
-number = st.number_input('国語', 0)
-number = st.number_input('数学', 0)
-number = st.number_input('社会', 0)
-number = st.number_input('理科', 0)
-number = st.number_input('英語', 0)
+number = st.number_input('国語', 50)
+number = st.number_input('数学', 50)
+number = st.number_input('社会', 50)
+number = st.number_input('理科', 50)
+number = st.number_input('英語', 50)
 
 
