6caa1425932266a257ca30d4250484e1cb82182b
lyios <muichiro2104@gmail.com>
2024-07-18 08:44:00 +0000

    ｇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import altair as alt
 import streamlit as st
+import matplotlib.pyplot as plt
 
 st.title('点数表示')
 
@@ -12,6 +13,24 @@ number = st.number_input('数学', 50)
 number = st.number_input('社会', 50)
 number = st.number_input('理科', 50)
 number = st.number_input('英語', 50)
+
+data = {
+    '科目': ['国語', '数学', '社会', '理科','英語'],
+    '点数': [10, 15, 7, 20]
+}
+df = pd.DataFrame(data)
+
+# Streamlitのタイトル
+st.title("カスタマイズされた棒グラフの例")
+
+# 棒グラフの作成
+plt.bar(df['科目'], df['点数'], color='blue')
+plt.xlabel('科目')
+plt.ylabel('点数')
+plt.title('科目ごとの点数')
+
+# グラフの表示
+st.pyplot(plt)
 
 
 
